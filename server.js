d3d1ce75d3292cf015e5f5a2b20ced7f3cc98a3e
MshariAsiri <109319432+MshariAsiri@users.noreply.github.com>
2025-04-23 03:31:44 +0300

    New bycrypt

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const { Pool } = require('pg');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const app = express();
 
 app.use(bodyParser.json());
